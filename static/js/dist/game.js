b0d6eecbb633ca0471d8e116199831d364b4417a
贵源 刘 <875079152@qq.com>
2023-03-04 15:59:20 +0800

    finish web acwing login

--- static/js/dist/game.js
+++ static/js/dist/game.js
@@ -551,7 +551,8 @@ class Settings {
         this.$register_login = this.$register.find(".ac-game-settings-option");
 
         this.$register.hide();
-
+		
+		this.$acwing_login = this.$settings.find('.ac-game-settings-acwing img');
         this.root.$ac_game.append(this.$settings);
 
         this.start();
@@ -563,8 +564,13 @@ class Settings {
     }
 
     add_listening_events() {
+        let outer = this;
         this.add_listening_events_login();
         this.add_listening_events_register();
+		this.$acwing_login.click(function() {
+            outer.acwing_login();
+        });
+
     }
 
     add_listening_events_login() {
@@ -587,6 +593,20 @@ class Settings {
             outer.register_on_remote();
         });
     }
+
+    acwing_login() {
+        $.ajax({
+            url: "https://app1506.acapp.acwing.com.cn/settings/acwing/web/apply_code/",
+            type: "GET",
+            success: function(resp) {
+                console.log(resp);
+                if (resp.result === "success") {
+                    window.location.replace(resp.apply_code_url);
+                }
+            }
+        });
+    }
+
 
     login_on_remote() {  // 在远程服务器上登录
         let outer = this;
